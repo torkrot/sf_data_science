ae61bd1ecc1d74560ad8279475d594ae5e3e4bb4
Krot <vova.krotov.2000@gmail.com>
2022-10-11 09:05:52 +0700

    Add Game

--- project_0/game.py
+++ project_0/game.py
@@ -4,14 +4,15 @@ number = np.random.randint(1,101)
 count = 0
 
 while True:
+    
     count += 1
     pred_num = int(input('Угадайте число от 1 до 100\n'))
     if pred_num > number:
         print('Это число меньше')
+        
     elif pred_num < number:
         print('Это число больше')
+    
     else:
         print(f'Поздравляем, вы угодали число {number} за {count} попыток')
         break
-
-print("hello world")
--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -2,21 +2,21 @@ import numpy as np
 #Игра в угодай число, где число будет угадывать сам ПК
 def rand_pred (number: int=1) -> int: #Создаем функцию
     
-    count = 0
-    pred_num = np.random.randint(1,101)
-    min_left = 1
-    max_right = 101
+    count = 0 # счетчик количества попыток
+    pred_num = np.random.randint(1,101) # генерация случайного числа
+    min_left = 1 # левая граница поиска
+    max_right = 101 # правая граница поиска
     
-    while pred_num != number:
+    while pred_num != number: # Цикл будет воспроизводиться пока число компьютера не равно загадоному
         
         count += 1
-        if pred_num < number:
-            min_left = pred_num
-            pred_num = ((min_left + max_right) // 2)
+        if pred_num < number: # Угадываем число если оно меньше
+            min_left = pred_num # Смещаем левую границу
+            pred_num = ((min_left + max_right) // 2) # следующие значение числа будет равно средней сумме левой и правое граници
             
-        elif pred_num > number:
-            max_right = pred_num
-            pred_num = ((min_left + max_right) // 2)   
+        elif pred_num > number: # Угадываем число если оно больше
+            max_right = pred_num # Смещаем правую границу
+            pred_num = ((min_left + max_right) // 2) # следующие значение числа будет равно средней сумме левой и правое граници  
             
     return count
 
@@ -29,12 +29,11 @@ def score_game(random_predict) -> int:
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls)) # реднее количество попыток
+    score = int(np.mean(count_ls)) # среднее количество попыток
 
     print(f'Алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
-# RUN
 if __name__ == '__main__':
-    
+    ##RUN
     score_game(rand_pred)
